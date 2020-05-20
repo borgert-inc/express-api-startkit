a8507f77b776cb8ddd122129dc02d7961630bf78
Odirlei Borgert <odirleiborgert@gmail.com>
2020-05-19 23:13:52 -0300

    Upgrade for slack

--- app/helpers/slack.js
+++ app/helpers/slack.js
@@ -2,18 +2,20 @@ var slack = require('slack')
 
 module.exports = (app) => {
     return {
-        postMessage: ({ message, channel }) => {
-            
-            if (!process.env.SLACK_TOKEN) return false
+        postMessage: (obj) => {
 
-            slack.chat.postMessage({
-                token: process.env.SLACK_TOKEN,
-                channel: (channel || process.env.SLACK_CHANNEL) || 'general',
-                text: message,
-                as_user: false,
-                username: 'My test',
-                icon_emoji: ':chart_with_upwards_trend:'
-            })
+            try {
+
+                if (!process.env.SLACK_TOKEN) return false
+
+                obj.token = process.env.SLACK_TOKEN
+                obj.channel = channel || process.env.SLACK_CHANNEL
+
+                slack.chat.postMessage(obj)
+
+            } catch (e) {
+                console.log('error', e.message)
+            }
 
         }
     }
