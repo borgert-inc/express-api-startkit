5fb4fa3367e94e2b8ed9c517e25f11b09ab982d9
Odirlei Borgert <odirleiborgert@gmail.com>
2021-03-26 08:36:30 -0300

    Add middleware

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,12 +3,12 @@ module.exports = (app) => {
 
     // Users controllers
     let controller = app.controllers.users
-    // let authMiddleware = app.middlewares.auth.auth
+    let authMiddleware = app.middlewares.auth.auth
 
-    app.get('/users', controller.index)
-    app.post('/users', controller.store)
-    app.get('/users/:id', controller.show)
-    app.put('/users/:id', controller.update)
-    app.delete('/users/:id', controller.destroy)
+    app.get('/users', authMiddleware, controller.index)
+    app.post('/users', authMiddleware, controller.store)
+    app.get('/users/:id', authMiddleware, controller.show)
+    app.put('/users/:id', authMiddleware, controller.update)
+    app.delete('/users/:id', authMiddleware, controller.destroy)
 
 }
